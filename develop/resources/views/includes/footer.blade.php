ddd1272c60c9fe0d4636516e463adb35468ef98a
Yangchi <chenade0312@gmail.com>
2022-09-26 18:39:03 +0800

    inital w/ link

--- develop/resources/views/includes/footer.blade.php
+++ develop/resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="col d-flex">
             <div class="mr-auto">
-                <p class="footer-content">Copyright &copy; Pete‘s Poker World 小P的撲克世界 <script>document.write(new Date().getFullYear());</script> All rights reserved | Design by YK</p>
+                <p class="footer-content">Copyright &copy; Pete‘s Poker World 小P的撲克世界 <script>document.write(new Date().getFullYear());</script> All rights reserved | Develop by <a href="https://www.linkedin.com/in/yangchi-kuo/" target="_blank">YK</a></p>
             </div>
             <div>
                 <a href="/privacy" target="_blank" style="color: #888888">{{trans('dictionary.privacy')}}</a>&emsp;
